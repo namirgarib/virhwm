9ce7cec88437086619c700f94eecd2d21043b8f7
Namir Garib <namirgarib@gmail.com>
2024-09-10 14:41:01 +0900

    Removed cached build files

--- archived/build/.qt/QtDeployTargets.cmake
+++
@@ -1,1 +0,0 @@
-set(__QT_DEPLOY_TARGET_simulator_FILE /Users/namirgarib/Desktop/nithc/E5/computer architecture/44garib-a/build/simulator)
--- cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,1 +0,0 @@
-set(__QT_DEPLOY_TARGET_virtual_hardware_m_FILE /Users/namirgarib/Desktop/Programming/cpp_programing/virtual-hardware-m/cmake-build-debug/virtual_hardware_m)
